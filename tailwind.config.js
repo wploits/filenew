e7177a553d790b65ce48f32c8115bf6550a56dbe
capped-_- <166282207+capped-uwu@users.noreply.github.com>
2025-01-31 13:25:21 +0900

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          750: '#2A2F3A',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
